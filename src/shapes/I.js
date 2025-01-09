9ea6a181b71c9d8bfc073199caab15affe35cc21
maladarix <maladarix@gmail.com>
2025-01-09 01:09:56 -0500

    Board

---
+++ src/shapes/I.js
@@ -0,0 +1,13 @@
+class IShape {
+  constructor() {
+    this.id = 1
+    this.name = "I"
+    this.color = "#17fff7"
+    this.shape = [
+      [1],
+      [1],
+      [1],
+      [1]
+    ]
+  }
+}
